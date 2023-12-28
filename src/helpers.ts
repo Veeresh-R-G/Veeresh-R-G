4518ef8fdd05921d9b7914a465a9a91ee5c7b491
Veeresh R G <veeresh.ravi@gmail.com>
2023-12-28 22:49:20 +0530

    [EDIT] Badge Corrections

--- src/helpers.ts
+++ src/helpers.ts
@@ -6,8 +6,10 @@ export const general = (array: string[]) => {
     return result;
 }
 
+
+
 export const codingPlatform = (R: Record<string, string>) => {
-    // Format for images : ![python](https://img.shields.io/badge/Python-3776AB?style=for-the-badge&logo=python&logoColor=white)
-    const result: string = Object.entries(R).map(([key,value]) => ` ![${key}](${value}) `).join("");
+// Format : [![Linkedin Badge](https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white)](${linkedinUrl})
+    const result: string = Object.entries(R).map(([key,value]) => ` [![${key}-Badge](${value})](${key}) `).join("");
     return result;
 }
